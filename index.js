14f5658e0e1b43ddaefd5520db7bfb156ae87a82
Will Hynes <williamhynes06@gmail.com>
2023-02-07 07:45:02 +0000

    modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ let root = document.documentElement;
 let spinQuant = Math.floor(screen.availHeight/180) * 180;
 console.log(spinQuant);
 
+let scrollPercent = () => {
+    let pos = document.documentElement.scrollTop;
+
+    let calcheight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+    let scrollvalue = Math.round( pos * 100 / calcheight);
+
+    return scrollvalue
+}
+
+
 root.style.setProperty('--spacer-amount', spinQuant + "px");
 
 window.addEventListener('scroll', (event) => {
